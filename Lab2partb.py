0c586376c5e2ac15006014a7c7ad2ce68b8c8141
BUIS305 <johnsona0731@students.bowiestate.edu>
2023-09-27 13:43:43 -0400

    Lab 2files

---
+++ Lab2partb.py
@@ -0,0 +1,18 @@
+number1 = 10
+number2 = 11
+number3 = 12
+number4 = 13
+
+add = number1 + number2 + number3 + number4
+mult = number1 * number2 * number3 * number4
+subtract = number4 - number3 - number2 - number1
+division = (number4/number3)
+
+print(add,mult,subtract,division)
+
+add = number1 + number2
+mult = number3 * number4
+subtract = number1 - number2
+division = number1/number2
+
+print(add,mult,subtract,division)
